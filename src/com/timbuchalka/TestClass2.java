4cbac2ae93e7e9eaa908fbd7c25452897a4c43b0
Arnold Graaff <arnoldgr@gmail.com>
2018-05-10 16:56:28 +0200

    committing changes

---
+++ src/com/timbuchalka/TestClass2.java
@@ -0,0 +1,9 @@
+package com.timbuchalka;
+
+public class TestClass2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(" a new test class 2 ");
+	}
+}
